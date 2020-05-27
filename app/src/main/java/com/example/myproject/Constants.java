4d14b5bb327d7ebb2d40005749b9e9c620338d36
kohashi20 <63204325+kohashi20@users.noreply.github.com>
2020-05-27 15:10:41 +0200

    Stockage des données en cache

---
+++ app/src/main/java/com/example/myproject/Constants.java
@@ -0,0 +1,5 @@
+package com.example.myproject;
+
+public class Constants {
+    static String KEY_MUSIC_LIST="jsonMusicList";
+}
--- app/src/main/java/com/example/myproject/MainActivity.java
+++ app/src/main/java/com/example/myproject/MainActivity.java
@@ -4,12 +4,16 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.widget.Toast;
 
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
+import com.google.gson.reflect.TypeToken;
 
+import java.lang.reflect.Type;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -25,12 +29,37 @@ public class MainActivity extends AppCompatActivity {
     private RecyclerView recyclerView;
     private ListAdapter mAdapter;
     private RecyclerView.LayoutManager layoutManager;
+    private SharedPreferences sharedPreferences;
+    private Gson gson;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         //showList();
-        makeAPICall();
+        sharedPreferences = getSharedPreferences("Music_Application", Context.MODE_PRIVATE);
+        gson = new GsonBuilder()
+                .setLenient()
+                .create();
+
+        List<Music> musicList = getDataFromCache();
+        if(musicList !=null){
+            showList(musicList);
+        } else {
+            makeAPICall();
+        }
+    }
+
+    private List<Music> getDataFromCache() {
+        String jsonMusic = sharedPreferences.getString(Constants.KEY_MUSIC_LIST, null);
+
+        if(jsonMusic == null){
+            return null;
+        }
+        else {
+            Type listType = new TypeToken<List<Music>>() {
+            }.getType();
+            return gson.fromJson(jsonMusic, listType);
+        }
     }
 
     private void showList(List<Music>musicList){
@@ -49,9 +78,6 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void makeAPICall(){
-        Gson gson = new GsonBuilder()
-                .setLenient()
-                .create();
 
         Retrofit retrofit = new Retrofit.Builder()
                 .baseUrl(BASE_URL)
@@ -66,6 +92,7 @@ public class MainActivity extends AppCompatActivity {
             public void onResponse(Call<RestMusicResponse> call, Response<RestMusicResponse> response) {
                 if (response.isSuccessful()&& response.body()!= null){
                     List<Music> musicList = response.body().getAlbum();
+                    saveList(musicList);
                     //Toast.makeText(MainActivity.this, "API success", Toast.LENGTH_SHORT).show();
                     showList(musicList);
                 }
@@ -80,6 +107,16 @@ public class MainActivity extends AppCompatActivity {
             }
         });
     }
+
+    private void saveList(List<Music> musicList) {
+        String jsonString= gson.toJson(musicList);
+        sharedPreferences
+                .edit()
+                .putString(Constants.KEY_MUSIC_LIST, jsonString)
+                .apply();
+        Toast.makeText(this, "List saved", Toast.LENGTH_SHORT).show();
+    }
+
     private void showError(){
         Toast.makeText(this, "API error", Toast.LENGTH_SHORT).show();
     }
