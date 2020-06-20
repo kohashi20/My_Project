66ceb42d6ec55f78efe1284f607ac4dbd6e6fdd7
Celine <lam@et.esiea.fr>
2020-06-20 16:22:05 +0200

    MVC architecture

--- app/src/main/java/com/example/myproject/Constants.java
+++ app/src/main/java/com/example/myproject/Constants.java
@@ -1,5 +1,6 @@
 package com.example.myproject;
 
 public class Constants {
+    public static final String BASE_URL = "https://www.theaudiodb.com/";
     public static String KEY_MUSIC_LIST="jsonMusicList";
 }
