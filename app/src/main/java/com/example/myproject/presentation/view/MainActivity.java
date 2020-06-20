be057f06e384adaeed89404bd2cd9a9c0146e1eb
kohashi20 <63204325+kohashi20@users.noreply.github.com>
2020-06-20 16:23:09 +0200

    Revert "MVC architecture"
    This reverts commit 66ceb42d

--- app/src/main/java/com/example/myproject/presentation/view/MainActivity.java
+++ app/src/main/java/com/example/myproject/presentation/view/MainActivity.java
@@ -12,7 +12,6 @@ import android.widget.Toast;
 import com.example.myproject.Constants;
 import com.example.myproject.R;
 import com.example.myproject.data.MusicAPI;
-import com.example.myproject.presentation.controller.MainController;
 import com.example.myproject.presentation.model.Music;
 import com.example.myproject.presentation.model.RestMusicResponse;
 import com.google.gson.Gson;
@@ -29,27 +28,44 @@ import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
 public class MainActivity extends AppCompatActivity {
+    private static final String BASE_URL = "https://www.theaudiodb.com/";
     private RecyclerView recyclerView;
     private ListAdapter mAdapter;
     private RecyclerView.LayoutManager layoutManager;
-
-    private MainController controller;
+    private SharedPreferences sharedPreferences;
+    private Gson gson;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        controller = new MainController(
         //showList();
-                this,
-           new GsonBuilder()
+        sharedPreferences = getSharedPreferences("Music_Application", Context.MODE_PRIVATE);
+        gson = new GsonBuilder()
                 .setLenient()
-                .create(),
-            getSharedPreferences("Music_Application", Context.MODE_PRIVATE)
-        );
-        controller.onStart();
+                .create();
+
+        List<Music> musicList = getDataFromCache();
+        if(musicList !=null){
+            showList(musicList);
+        } else {
+            makeAPICall();
+        }
     }
 
-    public void showList(List<Music>musicList){
+    private List<Music> getDataFromCache() {
+        String jsonMusic = sharedPreferences.getString(Constants.KEY_MUSIC_LIST, null);
+
+        if(jsonMusic == null){
+            return null;
+        }
+        else {
+            Type listType = new TypeToken<List<Music>>() {
+            }.getType();
+            return gson.fromJson(jsonMusic, listType);
+        }
+    }
+
+    private void showList(List<Music>musicList){
         recyclerView = (RecyclerView) findViewById(R.id.Recycle_View);
         // use this setting to
         // improve performance if you know that changes
@@ -64,7 +80,47 @@ public class MainActivity extends AppCompatActivity {
         recyclerView.setAdapter(mAdapter);
     }
 
-    public void showError(){
+    private void makeAPICall(){
+
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl(BASE_URL)
+                .addConverterFactory(GsonConverterFactory.create(gson))
+                .build();
+
+        MusicAPI musicAPI = retrofit.create(MusicAPI.class);
+
+        Call<RestMusicResponse> call = musicAPI.getMusicResponse();
+        call.enqueue(new Callback<RestMusicResponse>() {
+            @Override
+            public void onResponse(Call<RestMusicResponse> call, Response<RestMusicResponse> response) {
+                if (response.isSuccessful()&& response.body()!= null){
+                    List<Music> musicList = response.body().getAlbum();
+                    saveList(musicList);
+                    //Toast.makeText(MainActivity.this, "API success", Toast.LENGTH_SHORT).show();
+                    showList(musicList);
+                }
+                else{
+                    showError();
+                }
+            }
+
+            @Override
+            public void onFailure(Call<RestMusicResponse> call, Throwable t) {
+                showError();
+            }
+        });
+    }
+
+    private void saveList(List<Music> musicList) {
+        String jsonString= gson.toJson(musicList);
+        sharedPreferences
+                .edit()
+                .putString(Constants.KEY_MUSIC_LIST, jsonString)
+                .apply();
+        Toast.makeText(this, "List saved", Toast.LENGTH_SHORT).show();
+    }
+
+    private void showError(){
         Toast.makeText(this, "API error", Toast.LENGTH_SHORT).show();
     }
 }
