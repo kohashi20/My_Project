5cd96b6439029a4721b3e2f99c52ff1253cd750a
Celine <lam@et.esiea.fr>
2020-06-20 16:48:09 +0200

    Refactor packages MVC

--- app/src/main/java/com/example/myproject/Constants.java
+++ app/src/main/java/com/example/myproject/Constants.java
@@ -1,6 +1,5 @@
 package com.example.myproject;
 
 public class Constants {
-    public static final String BASE_URL = "https://www.theaudiodb.com/";
     public static String KEY_MUSIC_LIST="jsonMusicList";
 }
